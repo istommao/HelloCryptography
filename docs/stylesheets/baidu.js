ceab73d4e558c499808b0a0b40b84b1633d17faa
CodeMax <CodeMax@web3mq.com>
2023-01-15 16:49:20 +0800

    Add baidu analytics

---
+++ docs/stylesheets/baidu.js
@@ -0,0 +1,9 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?54e8fa017342fdd2b825eed1a1d0008d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
